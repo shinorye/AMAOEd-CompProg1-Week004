0091b3cc661ccb4302ed587af2772c84518365f6
shinorye <reivyann@hotmail.com>
2022-03-07 18:15:15 +0800

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <cstudio>
+#include <cstdlib>
+#include <iostream>
+#include "_pause.h"
+
+using namespace std;
+
+int main() {
+    string x;
+    cout << "Enter Section: ";
+    cin >> x;
+    cout << "***Section: " << x; cout << "***" << endl;
+
+    system ("pause");
+    return 0;
+}
